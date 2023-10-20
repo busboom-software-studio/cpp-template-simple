e8c5d3336b3c2ed6dbaa2e8c041e0fc193df5002
Eric Busboom <eric@civicknowledge.com>
2023-10-20 08:14:32 -0700

    .

--- tasks.py
+++ tasks.py
@@ -28,29 +28,27 @@ def on_rm_error(func, path, exc_info):
     chmod(path, S_IWRITE)
     unlink(path)
 
+def get_current_project_name(proj_dir: Path = Path.cwd()):
+    cmake_lists_path = proj_dir / "CMakeLists.txt"
+    with cmake_lists_path.open(encoding='utf-8') as file:
+        for line in file:
+            match = re.match(r'project\(([\w\d\-\_]+)', line)
+            if match:
+                return match.group(1)
+    raise ValueError("Project name not found in CMakeLists.txt")
 
-if len(sys.argv) != 2:
-    print("Please provide the project name as the only argument")
-    sys.exit(1)
-
-projectName = sys.argv[1]
 
 @task
-def rename(c, projectName):
+def name(c):
+    print(f"Project Name :'{get_current_project_name()}' " )
 
-    print("==> Changing the project name to {0}".format(projectName))
-    replaceTextInFile(
-        "{0}/CMakeLists.txt".format(projectName), "cpp-template", projectName)
-    replaceTextInFile(
-        "{0}/tools/build-win32-debug.bat".format(projectName), "cpp-template",
-        projectName)
-    replaceTextInFile(
-        "{0}/tools/build-win32-release.bat".format(projectName),
-        "cpp-template", projectName)
-    replaceTextInFile(
-        "{0}/tools/build-win64-debug.bat".format(projectName),
-        "cpp-template", projectName)
-    replaceTextInFile(
-        "{0}/tools/build-win64-release.bat".format(projectName),
-        "cpp-template", projectName)
+@task
+def rename(c, proj_name):
+    """Rename the project, chaning all of the CMake files where the project name appears. """
+
+    old_pn = get_current_project_name()
+
+    print("==> Changing the project name to {0}".format(proj_name))
+    replaceTextInFile( f"{proj_name}/CMakeLists.txt",old_pn, proj_name)
+
 
